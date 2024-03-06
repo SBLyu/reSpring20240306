1f1d02605bbbae8d1e5ab7feba4e334a84c65da4
sblyu <h0939605719@gmail.com>
2024-03-06 17:42:00 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/sblyu/reSpring20240306/ReSpring20240306Application.java
@@ -0,0 +1,13 @@
+package com.sblyu.reSpring20240306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReSpring20240306Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReSpring20240306Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sblyu/reSpring20240306/ReSpring20240306ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sblyu.reSpring20240306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReSpring20240306ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
