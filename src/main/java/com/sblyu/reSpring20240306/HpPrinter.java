ab6a8e3d347878aabdb881a633d760ae0ae02ffe
sblyu <h0939605719@gmail.com>
2024-03-08 17:37:24 +0800

    @PostConstruct Bean初始化

--- src/main/java/com/sblyu/reSpring20240306/HpPrinter.java
+++ src/main/java/com/sblyu/reSpring20240306/HpPrinter.java
@@ -1,11 +1,21 @@
 package com.sblyu.reSpring20240306;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class HpPrinter implements Printer {
 
+    private int count;
+
+    @PostConstruct
+    public void init(){
+        count=5;
+    }
+
     public void print(String message){
+        count--;
         System.out.println("Hp Printer: "+message);
+        System.out.println("count: "+count);
     }
 }
