b920ddf841e48a5d0c2dca0cf1b646132ede2c78
sblyu <h0939605719@gmail.com>
2024-03-11 15:23:17 +0800

    application.properties 設定初始值

--- src/main/java/com/sblyu/reSpring20240306/HpPrinter.java
+++ src/main/java/com/sblyu/reSpring20240306/HpPrinter.java
@@ -1,21 +1,21 @@
 package com.sblyu.reSpring20240306;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer {
 
-    private int count;
+    @Value("${printer.name}")
+    private String name;
 
-    @PostConstruct
-    public void init(){
-        count=5;
-    }
+    @Value("${printer.count:3}")
+    private int count;
 
     public void print(String message){
         count--;
-        System.out.println("Hp Printer: "+message);
+        System.out.println(name+" : "+message);
         System.out.println("count: "+count);
     }
 }
