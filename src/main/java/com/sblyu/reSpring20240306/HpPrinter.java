e61f0e733cd59d36eeea964eeb433d132c1e7f94
sblyu <h0939605719@gmail.com>
2024-03-12 11:39:27 +0800

    spring AOP test

--- src/main/java/com/sblyu/reSpring20240306/HpPrinter.java
+++ src/main/java/com/sblyu/reSpring20240306/HpPrinter.java
@@ -1,21 +1,13 @@
 package com.sblyu.reSpring20240306;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer {
 
-    @Value("${printer.name}")
-    private String name;
-
-    @Value("${printer.count:3}")
-    private int count;
 
     public void print(String message){
-        count--;
-        System.out.println(name+" : "+message);
-        System.out.println("count: "+count);
+        System.out.println("Hp Printer: "+message);
+
     }
 }
