53679ffb6784969a5c236f8ae8bbadef74751ad5
sblyu <h0939605719@gmail.com>
2024-03-21 16:48:33 +0800

    回傳 Json 格式

---
+++ src/main/java/com/sblyu/reSpring20240306/Store.java
@@ -0,0 +1,15 @@
+package com.sblyu.reSpring20240306;
+
+import java.util.List;
+
+public class Store {
+    List<String> productList;
+
+    public List<String> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<String> productList) {
+        this.productList = productList;
+    }
+}
