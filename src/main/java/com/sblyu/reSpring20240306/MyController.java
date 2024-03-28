82f142eceaa5819c8b7e627c1d411be181e77937
sblyu <h0939605719@gmail.com>
2024-03-28 10:51:13 +0800

    取得參數@RequestBody

--- src/main/java/com/sblyu/reSpring20240306/MyController.java
+++ src/main/java/com/sblyu/reSpring20240306/MyController.java
@@ -1,15 +1,17 @@
 package com.sblyu.reSpring20240306;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
     @RequestMapping("/test")
-    public String test(@RequestParam Integer id){
-        System.out.println("id: "+id);
+    public String test(@RequestBody Student student){
+
+        System.out.println("id: "+student.getId());
+        System.out.println("name: "+student.getName());
         return "test OK.";
     }
 
---
+++ src/main/java/com/sblyu/reSpring20240306/Student.java
@@ -0,0 +1,40 @@
+package com.sblyu.reSpring20240306;
+
+public class Student {
+
+    Integer id;
+    String name;
+
+//    public Integer getId(){
+//        return id;
+//    }
+//
+//    public void setId(Integer id){
+//        this.id=id;
+//    }
+//
+//    public String getName(){
+//        return name;
+//    }
+//
+//    public void setName(String name){
+//        this.name=name;
+//    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
