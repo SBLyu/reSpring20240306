2e45b624e83e5a6f9698644b8a72e785b592cc96
sblyu <h0939605719@gmail.com>
2024-03-28 10:29:01 +0800

    取得參數@RequestParam

--- src/main/java/com/sblyu/reSpring20240306/MyController.java
+++ src/main/java/com/sblyu/reSpring20240306/MyController.java
@@ -1,28 +1,16 @@
 package com.sblyu.reSpring20240306;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class MyController {
 
-    @RequestMapping("/product")
-    public Store product(){
-        Store store=new Store();
-        List<String> list=new ArrayList<>();
-        list.add("蘋果");
-        list.add("橘子");
-        store.setProductList(list);
-        return store;
+    @RequestMapping("/test")
+    public String test(@RequestParam Integer id){
+        System.out.println("id: "+id);
+        return "test OK.";
     }
 
-    @RequestMapping("/user")
-    public Student user(){
-        Student student=new Student();
-        student.setName("Judy");
-        return student;
-    }
 }
